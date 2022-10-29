cab9ca13c245f84172e08073c9ee79ecea2303b3
Ant1Dot0 <joselicla021@gmail.com>
2022-10-29 01:01:40 -0300

    creación modelo

---
+++ Modelo/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Producto
+    {
+        public int id { get; set; }
+        public string codigo { get; set; }
+        public string descripcion { get; set; }
+        public decimal ganancia { get; set; }
+        public decimal cantidad { get; set; }
+        public decimal precioCompra { get; set; }
+        public decimal stockMinimo { get; set; }
+        public decimal stock { get; set; }
+        public Tipo tipo { get; set; }
+        public Categoria categoria { get; set; }
+        public Marca marca { get; set; }
+    }
+}
---
+++ TpCuatrimestral-LiclaCisneros/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TpCuatrimestral_LiclaCisneros.Global" Language="C#" %>
